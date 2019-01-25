af0876a9a62c35df91cab193c71b0c6ae2367b03
Chris Houston <chouston@vizioz.com>
2019-01-25 09:33:08 -0500

    First version of the code, it's not working yet, currently the context menu option works, but I'm not able to get the current selected item.

---
+++ TaskIssues/Command1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace TaskIssues
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Command1
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("26b1b304-d5bb-4c05-b2dd-c4ed9fb35619");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// The current solution's GIT repository URL.
+        /// </summary>
+        private readonly string gitRepo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command1"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private Command1(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            this.gitRepo = GetGitUrl();
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in Command1's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+
+            Instance = new Command1(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get a reference to the Development Tools Environment (DTE) - I.e. the main VS Application
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+            Assumes.Present(dte);
+
+            // Get the current selected task item
+            var item = GetTaskItem(dte);
+            
+            if (item == null)
+            {
+                VsShellUtilities.ShowMessageBox(
+                serviceProvider: this.package,
+                title: "Task Issues",
+                message: "No GITHub issue was found in the task definition",
+                icon: OLEMSGICON.OLEMSGICON_INFO,
+                msgButton: OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                defaultButton: OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+            else
+            {
+                // TODO: #3 - This will need to be changed to use the GITHUB API to pull in the Issue info to a custom UI within Visual Studio as GitHub does not support IE which is the embedded browser!
+                // Open a browser window with the GIT issue
+                var itemOps = dte.ItemOperations;
+                itemOps.Navigate(gitRepo + item);
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Command1 Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static string GetTaskItem(DTE2 dte)
+        {
+            var regex = new Regex(@"\W(\#[0-9]+\b)(?!;)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            // TODO: #2 - This function should be getting the current selected item, instead it is currently returned the first item in the list.
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var items = dte.ToolWindows.TaskList.TaskItems;
+
+            var itemList = items.Cast<TaskItem>();
+            if (!itemList.Any())
+            {
+                return string.Empty;
+            }
+
+            MatchCollection matches = regex.Matches(itemList.FirstOrDefault<TaskItem>().Description);
+
+            if (matches.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            return matches[0].Value.Substring(2);
+        }
+
+        public string GetGitUrl()
+        {
+            // TODO: #1 - Get the current repository's GIT URL from the GIT config file
+            return "https://github.com/Vizioz/task-issues/issues/";
+        }
+
+        public bool HasGitUrl()
+        {
+            return string.IsNullOrEmpty(gitRepo) ? false : true;
+        }
+    }
+}
